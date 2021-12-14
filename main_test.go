c12719938ebec1cf796b21eef08f44b25221909b
Vikas <vik@Vikass-MacBook-Pro.local>
2021-12-14 08:41:13 +0530

    strategy

--- main_test.go
+++ main_test.go
@@ -31,3 +31,13 @@ func Test(t *testing.T) {
 		}
 	}
 }
+
+// feature - [ToUpper, ToCamel]
+
+// staging - [staging.swiftbit.xyz] - [experimental] -/ [nightly, experimental]
+
+// testing - [testing.swiftbit.xyz] - [ready to test] -
+
+// [release - 0.0.1, 0.0.2, 0.0.3, 0.0.4, 0.1.0, 0.1.1]
+
+// production - 0.1.0 [admin.swiftbit.xyz]
